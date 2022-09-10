afc7d4c0f697a4a2133443fb0d0b678c005ff649
Cher-iF <112823662+Cher-iF@users.noreply.github.com>
2022-09-11 01:33:35 +0200

    Add files via upload

---
+++ test_deploy.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 11 01:28:16 2022
+
+@author: bot
+"""
+
+import streamlit as st
+
+st.sidebar.header("""
+                  P7 Project on Sentiment Analysis
+                  """)   
+                  
+st.header("""
+                  P7 Project on Sentiment Analysis
+                  """)  
